acf43d16cac90209e661fc96667107b6debc90d7
Himanshu <himanshu.kumar@oodlestechnologies.com>
2017-09-11 13:01:05 +0530

    resolved issue related to user controller

--- src/main/java/com/bolenum/controller/user/UserController.java
+++ src/main/java/com/bolenum/controller/user/UserController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolenum.model.User;
-import com.bolenum.constant.Message;
 import com.bolenum.constant.UrlConstant;
-import com.bolenum.util.ResponseHandler;
-
 import io.swagger.annotations.Api;
 
 /**
@@ -29,29 +25,13 @@ import io.swagger.annotations.Api;
 @RequestMapping(value = UrlConstant.BASE_USER_URI_V1)
 @Api(value = "User Controller", description = "User releated methods")
 public class UserController {
-	
+
 	public static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-	 @RequestMapping(value = "/adduser", method = RequestMethod.GET)
-	 public ResponseEntity<Object> addUser(@Valid @RequestBody User user, BindingResult result) {
-		
-		 if(result.hasErrors())
-		 {
-			 return ResponseHandler.response(HttpStatus.BAD_REQUEST, true, Message.USER_INVALID_EMAIL, null); 
-		 }
-		 else
-		 {
-			  /*Boolean isUserExist = userService.userIsExist(user);
-			  if (!isUserExist)
-			  {
-				  
-			  }
-			  else
-			  {
-				  
-			  }*/
-			  return null;
-		 }
-	
+	@RequestMapping(value = "/adduser", method = RequestMethod.GET)
+	public ResponseEntity<Object> addUser(@Valid @RequestBody User user, BindingResult result) {
+
+		return null;
 	}
+
 }
