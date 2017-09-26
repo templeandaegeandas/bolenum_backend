90b3a9ec00bf248671f9e02cb0ab02fcd803b63d
Chandan <chandan.kumar@oodlestechnologies.com>
2017-09-26 15:22:20 +0530

    added ethereum dependency

---
+++ src/main/java/com/bolenum/services/user/wallet/EtherumWalletService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bolenum.services.user.wallet;
+
+/**
+ * @author chandan kumar singh
+ * @date 26-Sep-2017
+ */
+public interface EtherumWalletService {
+	/**
+	 * 
+	 * @description createWallet
+	 * @param 
+	 * @return String
+	 * @exception 
+	 *
+	 */
+	String createWallet(String uuid);
+}
---
+++ src/main/java/com/bolenum/services/user/wallet/EtherumWalletServiceImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bolenum.services.user.wallet;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author chandan kumar singh
+ * @date 26-Sep-2017
+ */
+@Service
+public class EtherumWalletServiceImpl implements EtherumWalletService {
+
+	@Override
+	public String createWallet(String uuid) {
+		return null;
+	}
+
+}
