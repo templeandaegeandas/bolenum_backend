cfbc87484ec7fd4765da8da89249d280bcf1887f
Chandan <chandan.kumar@oodlestechnologies.com>
2017-10-16 12:03:20 +0530

    implemented transaction api for orders

--- src/main/java/com/bolenum/services/user/transactions/TransactionService.java
+++ src/main/java/com/bolenum/services/user/transactions/TransactionService.java
@@ -3,10 +3,13 @@
  */
 package com.bolenum.services.user.transactions;
 
+import com.bolenum.model.User;
+
 /**
  * @author chandan kumar singh
  * @date 29-Sep-2017
  */
 public interface TransactionService {
-	public void saveEthereumIncomingTx();
+	public boolean performEthTransaction(User fromUser, String toAddress, Double amount);
+	public boolean performBtcTransaction(User fromUser, String toAddress, Double amount);
 }
