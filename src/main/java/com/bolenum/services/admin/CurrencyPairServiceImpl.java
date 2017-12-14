953c7362b76071305b65aa8ec22c81ce38c026a3
Chandan <chandan.kumar@oodlestechnologies.com>
2017-12-14 15:52:25 +0530

    eth/btc trancation issue fixed

--- src/main/java/com/bolenum/services/admin/CurrencyPairServiceImpl.java
+++ src/main/java/com/bolenum/services/admin/CurrencyPairServiceImpl.java
@@ -2,8 +2,6 @@ package com.bolenum.services.admin;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
