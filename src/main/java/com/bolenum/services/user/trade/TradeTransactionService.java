9e62d63a201f784dcfe53b85e9b8fa7ed0494084
Chandan <chandan.kumar@oodlestechnologies.com>
2017-12-20 17:03:32 +0530

    btc move account balance

--- src/main/java/com/bolenum/services/user/trade/TradeTransactionService.java
+++ src/main/java/com/bolenum/services/user/trade/TradeTransactionService.java
@@ -10,11 +10,14 @@ import com.bolenum.model.User;
  * @date 19-Dec-2017
  */
 public interface TradeTransactionService {
-	public Boolean performTradeTransaction(String currencyAbr, double qtyTraded, User buyer, User seller, boolean isFee,
-			Long tradeId);
+	public Boolean performTradeTransaction(String currencyAbr, String currencyType, double qtyTraded, User buyer,
+			User seller, Long tradeId);
+	public Boolean performTradeTransactionFee(String currencyAbr, String currencyType, double tradeFee, User buyer,
+			User seller, Long tradeId);
 
 	public Boolean performErc20Trade(User seller, String currencyAbr, User buyer, double qtyTraded, Long tradeId);
+
 	public Boolean performEthTrade(User seller, String currencyAbr, User buyer, double qtyTraded, Long tradeId);
 
-	public Boolean performBtcTrade(User seller, String currencyAbr, User buyer, double qtyTraded, Long tradeId);
+	public Boolean performBtcTrade(User fromUser, User toUser, double qtyTraded, Long tradeId);
 }
--- src/main/java/com/bolenum/services/user/trade/TradeTransactionServiceImpl.java
+++ src/main/java/com/bolenum/services/user/trade/TradeTransactionServiceImpl.java
@@ -3,17 +3,24 @@
  */
 package com.bolenum.services.user.trade;
 
+import java.math.BigDecimal;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bolenum.exceptions.InsufficientBalanceException;
 import com.bolenum.model.User;
 import com.bolenum.model.coin.UserCoin;
 import com.bolenum.repo.common.coin.UserCoinRepository;
-import com.bolenum.services.admin.CurrencyService;
 import com.bolenum.services.common.coin.Erc20TokenService;
 import com.bolenum.services.user.notification.NotificationService;
 import com.bolenum.util.GenericUtils;
+import com.bolenum.util.ResourceUtils;
+import com.neemre.btcdcli4j.core.BitcoindException;
+import com.neemre.btcdcli4j.core.CommunicationException;
+import com.neemre.btcdcli4j.core.client.BtcdClient;
 
 /**
  * @author chandan kumar singh
@@ -21,10 +28,7 @@ import com.bolenum.util.GenericUtils;
  */
 @Service
 public class TradeTransactionServiceImpl implements TradeTransactionService {
-	private static Logger logger = org.slf4j.LoggerFactory.getLogger(TradeTransactionServiceImpl.class);
-
-	@Autowired
-	private CurrencyService currencyService;
+	private static Logger logger = LoggerFactory.getLogger(TradeTransactionServiceImpl.class);
 
 	@Autowired
 	private NotificationService notificationService;
@@ -34,45 +38,35 @@ public class TradeTransactionServiceImpl implements TradeTransactionService {
 	@Autowired
 	private Erc20TokenService erc20TokenService;
 
+	/**
+	 * this will perform trade transaction for buyer to seller
+	 */
 	@Override
-	public Boolean performTradeTransaction(String currencyAbr, double qtyTraded, User buyer, User seller, boolean isFee,
-			Long tradeId) {
+	public Boolean performTradeTransaction(String currencyAbr, String currencyType, double qtyTraded, User buyer,
+			User seller, Long tradeId) {
 
-		String currencyType = currencyService.findByCurrencyAbbreviation(currencyAbr).getCurrencyType().toString();
-		String msg = "";
-		String msg1 = "";
-		logger.debug("perform transaction for admin fee: {}", isFee);
-		if (!isFee) {
-			msg = "Hi " + seller.getFirstName() + ", Your transaction of selling "
-					+ GenericUtils.getDecimalFormatString(qtyTraded) + " " + currencyAbr
-					+ " have been processed successfully!";
-			msg1 = "Hi " + buyer.getFirstName() + ", Your transaction of buying "
-					+ GenericUtils.getDecimalFormatString(qtyTraded) + " " + currencyAbr
-					+ " have been processed successfully!";
-		}
+		String msg = "Hi " + seller.getFirstName() + ", Your transaction of selling "
+				+ GenericUtils.getDecimalFormatString(qtyTraded) + " " + currencyAbr
+				+ " have been processed successfully!";
+		String msg1 = "Hi " + buyer.getFirstName() + ", Your transaction of buying "
+				+ GenericUtils.getDecimalFormatString(qtyTraded) + " " + currencyAbr
+				+ " have been processed successfully!";
 		Boolean txStatus;
 		switch (currencyType) {
 		case "CRYPTO":
 			if (currencyAbr == "BTC") {
 				logger.debug("BTC trade started");
-				txStatus = performBtcTrade(seller,currencyAbr, buyer, qtyTraded, tradeId);
+				txStatus = performBtcTrade(seller, buyer, qtyTraded, tradeId);
 				logger.debug("is BTC trade successed: {}", txStatus);
 				/**
 				 * if trade for users, then return result with mail notification
 				 * to users
 				 */
-				if (txStatus && !isFee) {
+				if (txStatus) {
 					notificationService.sendNotification(seller, msg);
 					notificationService.saveNotification(buyer, seller, msg);
 					notificationService.sendNotification(buyer, msg1);
 					notificationService.saveNotification(buyer, seller, msg1);
-					return true;
-				}
-				/**
-				 * if transaction for admin, then return result without mail
-				 * notification
-				 */
-				if (txStatus && isFee) {
 					return true;
 				}
 			} else if (currencyAbr == "ETH") {
@@ -83,20 +77,12 @@ public class TradeTransactionServiceImpl implements TradeTransactionService {
 				 * if transaction for users, then return result with mail
 				 * notification to users
 				 */
-				if (txStatus && !isFee) {
+				if (txStatus) {
 					notificationService.sendNotification(seller, msg);
 					notificationService.saveNotification(buyer, seller, msg);
 					notificationService.sendNotification(buyer, msg1);
 					notificationService.saveNotification(buyer, seller, msg1);
-					logger.debug("Message : {}", msg);
-					logger.debug("Message : {}", msg1);
-					return true;
-				}
-				/**
-				 * if transaction for admin, then return result without mail
-				 * notification
-				 */
-				if (txStatus && isFee) {
+					logger.debug("Message : {} , Message1: {}", msg, msg1);
 					return true;
 				}
 			}
@@ -109,20 +95,13 @@ public class TradeTransactionServiceImpl implements TradeTransactionService {
 			 * if transaction for users, then return result with mail
 			 * notification to users
 			 */
-			if (txStatus && !isFee) {
+			if (txStatus) {
 				notificationService.sendNotification(seller, msg);
 				notificationService.saveNotification(buyer, seller, msg);
 				notificationService.sendNotification(buyer, msg1);
 				notificationService.saveNotification(buyer, seller, msg1);
 				logger.debug("Message : {}", msg);
 				logger.debug("Message : {}", msg1);
-				return txStatus;
-			}
-			/**
-			 * if transaction for admin, then return result without mail
-			 * notification
-			 */
-			if (txStatus && isFee) {
 				return txStatus;
 			}
 			break;
@@ -189,7 +168,7 @@ public class TradeTransactionServiceImpl implements TradeTransactionService {
 	 */
 	@Override
 	public Boolean performEthTrade(User seller, String currencyAbr, User buyer, double qtyTraded, Long tradeId) {
-		return null;
+		return false;
 	}
 
 	/**
@@ -203,7 +182,64 @@ public class TradeTransactionServiceImpl implements TradeTransactionService {
 	 * 
 	 */
 	@Override
-	public Boolean performBtcTrade(User seller, String currencyAbr, User buyer, double qtyTraded, Long tradeId) {
-		return null;
+	public Boolean performBtcTrade(User fromUser, User toUser, double qtyTraded, Long tradeId) {
+		try {
+			BtcdClient client = ResourceUtils.getBtcdProvider();
+			BigDecimal currentBal = client.getBalance(String.valueOf(fromUser.getUserId()));
+			BigDecimal balance = BigDecimal.valueOf(qtyTraded);
+			logger.debug("user: {} has current account balance:{} and trade amount: {}", fromUser.getEmailId(),
+					GenericUtils.getDecimalFormatString(currentBal.doubleValue()),
+					GenericUtils.getDecimalFormatString(balance.doubleValue()));
+			if (currentBal.compareTo(balance) > 0) {
+				throw new InsufficientBalanceException("You have insufficent balance");
+			}
+			boolean res = client.move(fromUser.getUserId().toString(), toUser.getUserId().toString(), balance);
+			if (res) {
+				logger.info("from user: {} to user: {} amount: {} for tradeId: {} success", fromUser.getEmailId(),
+						toUser.getEmailId(), GenericUtils.getDecimalFormatString(balance.doubleValue()), tradeId);
+				return res;
+			}
+			logger.error("from user: {} to user: {} amount: {} for tradeId: {} failed", fromUser.getEmailId(),
+					toUser.getEmailId(), GenericUtils.getDecimalFormatString(balance.doubleValue()), tradeId);
+		} catch (BitcoindException | CommunicationException e) {
+			logger.error("BTC account creation error: {}", e);
+		}
+		return false;
+	}
+
+	@Override
+	public Boolean performTradeTransactionFee(String currencyAbr, String currencyType, double tradeFee, User buyer,
+			User seller, Long tradeId) {
+		Boolean txStatus;
+		switch (currencyType) {
+		case "CRYPTO":
+			if (currencyAbr == "BTC") {
+				logger.debug("BTC trade fee started");
+				txStatus = performBtcTrade(seller, buyer, tradeFee, tradeId);
+				logger.debug("is BTC trade fee successed: {}", txStatus);
+				if (txStatus) {
+					return true;
+				}
+			} else if (currencyAbr == "ETH") {
+				logger.debug("ETH trade fee started");
+				txStatus = performEthTrade(seller, currencyAbr, buyer, tradeFee, tradeId);
+				logger.debug("is ETH trade fee successed: {}", txStatus);
+				if (txStatus) {
+					return true;
+				}
+			}
+			break;
+		case "ERC20TOKEN":
+			logger.debug("ERC20TOKEN trade fee started");
+			txStatus = performErc20Trade(seller, currencyAbr, buyer, tradeFee, tradeId);
+			logger.debug("is ERC20TOKEN trade fee successed: {}", txStatus);
+			if (txStatus) {
+				return txStatus;
+			}
+			break;
+		default:
+			break;
+		}
+		return false;
 	}
 }
